a6c1b3244fbc42c50d88f7924a5d259bdf650e7b
andreadelrio <delrio.andre@gmail.com>
2016-02-25 23:23:07 +0200

    bg image

--- sites/all/themes/fpi2/templates/node--8.tpl.php
+++ sites/all/themes/fpi2/templates/node--8.tpl.php
@@ -72,8 +72,11 @@ $resultPrinciples = $result0->fetchAllAssoc('nid');
 
 			<?php if ($rowNumber == 1 && $node->language == $lang_name): ?> 
 				 <li data-fpi-num=<?php print $node->field_number_value; ?> class="text-center">
-				 	<h1><?php print $node->field_featured_image_fid; ?></h1>
-				 	<div style="background-image: url('#');background-repeat: no-repeat;background-size: contain;background-position: center;">
+				 	<?php 
+				 		$file = file_load($node->field_featured_image_fid);
+				 		$route = "sites/default/files/".$file->filename;
+				 	?>
+				 	<div style="background-image: url(<?php print_r($route); ?>);background-repeat: no-repeat;background-size: contain;background-position: center;">
 				 		<h4 class="text-center"><?php print $node->field_number_value; ?>. <?php print $node->title; ?></h4>
 				 		<i class="ion-plus-circled x2"></i>
 				 	</div>
