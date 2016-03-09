62071ff23e6395d13e7ef17b454249ffdf324e0f
andreadelrio <delrio.andre@gmail.com>
2016-03-09 13:18:01 -0500

    events

--- sites/all/themes/fpi2/templates/views-view-fields--events.tpl.php
+++ sites/all/themes/fpi2/templates/views-view-fields--events.tpl.php
@@ -1,8 +1,8 @@
 <div class="event mb-14 row">
-	<div class="columns medium-3">
+	<div class="columns medium-4">
 		<?php print $fields["field_featured_image"]->content; ?>
 	</div>
-	<div class="columns medium-9">
+	<div class="columns medium-8">
 		<?php print $fields["title"]->content; ?>
 		<?php echo substr($fields["field_description"]->content, 0, 300); ?>
 	</div>
