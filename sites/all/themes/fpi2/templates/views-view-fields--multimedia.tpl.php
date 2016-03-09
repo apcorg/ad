7517078749b9387ec1f794880c7283912ce17ff2
andreadelrio <delrio.andre@gmail.com>
2016-03-09 06:33:53 -0500

    new multimedia

--- sites/all/themes/fpi2/templates/views-view-fields--multimedia.tpl.php
+++ sites/all/themes/fpi2/templates/views-view-fields--multimedia.tpl.php
@@ -1,7 +1,11 @@
 <li>
   <a href=/node/<?php print $fields["nid"]->content; ?>>
-		<?php print $fields["field_asset"]->content; ?>
-		<?php print $fields["field_image"]->content; ?>
-		<?php print $fields["title"]->content; ?>
+  	<div class="image">
+			<?php print $fields["field_asset"]->content; ?>
+			<?php print $fields["field_image"]->content; ?>
+  	</div>
   </a>
+  <div class="asset-info p-7">
+		<?php print $fields["title"]->content; ?>
+  </div>
 </li>
