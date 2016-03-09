7c2ffb7ed2249c8ff9b4bbe000cf3853c92fb586
andreadelrio <delrio.andre@gmail.com>
2016-03-09 09:18:12 -0500

    multimedia form

--- sites/all/modules/custom_content_forms/multimedia-node-form.tpl.php
+++ sites/all/modules/custom_content_forms/multimedia-node-form.tpl.php
@@ -1,4 +1,7 @@
-<h5>Add a new item to the multimedia gallery.</h5>
-<?php
-$form=drupal_get_form('multimedia_node_form');
-print drupal_render($form);
+<h5 class="montse">Add a new item to the multimedia gallery.</h5>
+<div class="test">
+	<?php
+	$form=drupal_get_form('multimedia_node_form');
+	print drupal_render($form);
+	?>
+</div>
