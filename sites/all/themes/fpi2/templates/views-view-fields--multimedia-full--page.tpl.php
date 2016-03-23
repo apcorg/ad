f48b21a5ab637d0f453d1ef15dcf75e850f2d1ba
andreadelrio <delrio.andre@gmail.com>
2016-03-23 16:13:34 -0700

    multimedia single

--- sites/all/themes/fpi2/templates/views-view-fields--multimedia-full--page.tpl.php
+++ sites/all/themes/fpi2/templates/views-view-fields--multimedia-full--page.tpl.php
@@ -1,7 +1,7 @@
 <div class="row pbp-7">
 	<h4 class="text-center montse"><?php print $fields['title']->content;?></h4>
 	<div class="columns medium-10 medium-offset-1 pt-28 pb-28">
-		<?php print $fields['field_mm_description']->content; ?>
+		<p class="text-justify"><?php print $fields['field_mm_description']->content; ?></p>
 		<div class="text-center pt-28">
 			<?php if (strpos($fields['field_media_type']->content,'Audio') > 0): ?>
 			<?php print $fields['field_asset']->content; ?>
