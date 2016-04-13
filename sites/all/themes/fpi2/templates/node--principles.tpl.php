4d7d8622f9636f970bdde2c797851f58119c8023
andreadelrio <delrio.andre@gmail.com>
2016-04-13 12:01:40 -0500

    principle template

--- sites/all/themes/fpi2/templates/node--principles.tpl.php
+++ sites/all/themes/fpi2/templates/node--principles.tpl.php
@@ -1,9 +1,10 @@
 <h3 class="montse"><?php print t('Application of the principles') ?></h3>
-<div class="see-principle mb-28 clearfix">
+<div class="see-principle mt-28 mb-28 clearfix">
 	<?php 
 		$number = $node->field_number['und'][0]['value'];
 		$f_image = file_create_url($node->field_featured_image['und'][0]['uri']);
 		$title_1 = $node->title;
+		$background = $node->field_background['und'][0]['value'];
 		print '<div class="clearfix">';
 		print_r( '<div class="pull-left"><h4 class="montse">'.$number.'.&nbsp;&nbsp;'.'</h4></div>'); 
 		print_r( '<div class="pull-left"><h4 class="montse">'.$title_1.'</h4></div>'); 
@@ -16,5 +17,10 @@
 		<div class="medium-9 columns">
 			<?php print render($content['body']); ?>
 		</div>
+	</div>
+</div>
+<div class="row">
+	<div class="small-12 columns">
+		<?php print_r($background); ?>
 	</div>
 </div>
