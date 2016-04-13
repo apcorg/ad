c1f3b73c8e427f4339f07eaddf7e0d74af6401fb
andreadelrio <delrio.andre@gmail.com>
2016-04-13 11:37:07 -0500

    principle template

--- sites/all/themes/fpi2/templates/node--principles.tpl.php
+++ sites/all/themes/fpi2/templates/node--principles.tpl.php
@@ -2,11 +2,12 @@
 	<?php 
 		$number = $node->field_number['und'][0]['value'];
 		$f_image = file_create_url($node->field_featured_image['und'][0]['uri']);
+		$title_1 = $node->title;
+		print '<div class="clearfix">';
+		print_r( '<div class="pull-left"><h4 class="montse">'.$number.'.&nbsp;&nbsp;'.'</h4></div>'); 
+		print_r( '<div class="pull-left"><h4 class="montse">'.$title_1.'</h4></div>'); 
+		print '</div>';
 	?>
-	<div class="clearfix">
-		<div class="pull-left"><h4 class="montse"></h4><?php print $number; ?>.&nbsp;&nbsp;</div>
-		<div class="pull-left"><h4 class="montse"></h4><?php print $title; ?></div>
-	</div>
 	<div class="row">
 		<div class="medium-3 columns">
 			<img src="<?php print $f_image; ?>" alt="">
