e84603ce801afd7ca6385f456006e35670a44732
andreadelrio <delrio.andre@gmail.com>
2016-04-18 09:54:32 -0500

    ctas

--- sites/all/themes/fpi2/templates/node--8.tpl.php
+++ sites/all/themes/fpi2/templates/node--8.tpl.php
@@ -232,8 +232,8 @@ $resultPrinciples = $result0->fetchAllAssoc('nid');
 		 		    <a target="_blank" href="http://twitter.com/home?status=%22<?php echo substr($node->body_value, 0, 97); ?>...%22%20<?php print t('Learn more at africaninternetrights.net/') ?>"><i class="ion-social-twitter"></i></a>
 		 		  </div>
 		 		  <div class="ctas-grid text-center">
-		 		    <a class="cta-button mt-14" href=/node/<?php print $node->nid; ?>><i class="ion-information-circled bold-icon x2"></i><span>&nbsp;<?php print t('Application') ?></span></a>
-		 		    <a class="cta-button" href=/see-principle/<?php print $node->nid; ?>><i class="ion-ios-list-outline bold-icon x2"></i><span>&nbsp;<?php print t('Relevant resources') ?></span></a>
+		 		    <a class="cta-button" href=/node/<?php print $node->nid; ?>><i class="ion-information-circled bold-icon x2"></i><span>&nbsp;<?php print t('Application') ?></span></a>
+		 		    <a class="cta-button mt-14" href=/see-principle/<?php print $node->nid; ?>><i class="ion-ios-list-outline bold-icon x2"></i><span>&nbsp;<?php print t('Relevant resources') ?></span></a>
 		 		    <a class="cta-button mt-14" href=/see-principle/<?php print $node->nid; ?>><i class="ion-chatboxes x2"></i><span>&nbsp;<?php print t('Join the conversation') ?></span></a>
 		 		  </div>
 			 	</div>
