8c1ca37da171255982d2097be9837a9bbed80481
andreadelrio <delrio.andre@gmail.com>
2016-04-18 16:29:48 -0500

    event single

--- sites/all/themes/fpi2/templates/node--event.tpl.php
+++ sites/all/themes/fpi2/templates/node--event.tpl.php
@@ -1,14 +1,12 @@
-<div class="row">
-	<h3 class="montse text-center"><?php print $title; ?></h3>
-	<p class="text-center"><?php print t('Submitted by') ?> <?php print($node->name); ?></p>
-	<div class="text-center">
-		<?php print render($content['field_featured_image']); ?>
-	</div>
-	<div class="columns medium-10 medium-offset-1 pt-28 pb-28">
-		<?php 
-		print render($content['body']); 
-		print render($content['field_place_of_the_event']); 
-		print render($content['field_event_date']); 
-		?>
-	</div>
+<h3 class="montse text-center"><?php print $title; ?></h3>
+<p class="text-center"><?php print t('Submitted by') ?> <?php print($node->name); ?></p>
+<div class="text-center">
+	<?php print render($content['field_featured_image']); ?>
+</div>
+<div class="columns medium-10 medium-offset-1 pt-28 pb-28">
+	<?php 
+	print render($content['body']); 
+	print render($content['field_place_of_the_event']); 
+	print render($content['field_event_date']); 
+	?>
 </div>
